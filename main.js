e46ccf35083596ff2e9d90aea78d47f90c809c88
siobahn <siobahnschuck@gmail.com>
2017-10-31 11:25:23 -0500

    all the files are here

---
+++ main.js
@@ -0,0 +1,59 @@
+
+var image = document.createElement('img');
+
+function ranColor(){
+  var r = Math.random() * 255;
+  var g = Math.random() * 255;
+  var b = Math.random() * 255;
+  var a = Math.random() * 1;
+  //return 'rgb('+r+', '+g+', '+b+')';
+  return `rgb(${r}, ${g}, ${b}, ${a})`;
+}
+
+var canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+
+// some hotfixes... ( ≖_≖)
+document.body.style.margin = 0;
+canvas.style.position = 'fixed';
+
+// get canvas 2D context and set him correct size
+var ctx = canvas.getContext('2d');
+resize();
+
+// last known position
+var pos = { x: 0, y: 0 };
+
+window.addEventListener('resize', resize);
+document.addEventListener('mousemove', draw);
+document.addEventListener('mousedown', setPosition);
+document.addEventListener('mouseenter', setPosition);
+
+// new position from mouse event
+function setPosition(e) {
+  pos.x = e.clientX;
+  pos.y = e.clientY;
+}
+
+// resize canvas
+function resize() {
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+}
+
+function draw(e) {
+  // mouse left button must be pressed
+  if (e.buttons !== 1) return;
+
+  ctx.beginPath(); // begin
+
+  ctx.lineWidth = 5;
+  ctx.lineCap = 'round';
+  ctx.strokeStyle = ranColor();
+
+  ctx.moveTo(pos.x, pos.y); // from
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y); // to
+
+  ctx.stroke(); // draw it!
+}
